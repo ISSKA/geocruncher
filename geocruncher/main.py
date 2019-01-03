7badf4311b9aadc9b49508c48c2f22154002ce79
Paul Berclaz <38962649+poluxxs@users.noreply.github.com>
2019-01-03 13:59:10 +0100

    Update main.py

--- geocruncher/main.py
+++ geocruncher/main.py
@@ -64,7 +64,7 @@ def run_geocruncher(args):
         # TODO do something useful with output files
         print(generated_mesh_paths)
 
-    if args[1] == 'surface':
+    if args[1] == 'slice':
         nPoints=500
         data = json.loads(args[2])
         xCoord=np.array([data["start"]["x"], data["end"]["x"]])
@@ -76,11 +76,8 @@ def run_geocruncher(args):
         outputs = {'forSurface': CrossSectionSurface.output(xCoord,yCoord,zCoord,nPoints,model, [1, 1])} 
         (rank) = CrossSectionSurface.output(xCoord,yCoord,zCoord,nPoints,model, [1, 1])
         outputs = { 'rank': rank }
-        #import codecs
-        #json.dump(outputs,codecs.open('output.json', 'w', encoding='utf-8'), indent = 2, separators=(',', ': '))    
         with open(args[5], 'w') as f:
             json.dump(outputs, f, indent = 2, separators=(',', ': '))        
-        sys.stdout.flush()
 
 def isOutofBounds(xCoord, yCoord, box):
     if xCoord > box.xmax or xCoord < box.xmin or yCoord > box.ymax or yCoord < box.ymin:
