1d55b13443e4771347eaa3ed1246f55a0fe700d6
Kevin Bütikofer <kevin.butikofer@outlook.com>
2021-08-02 16:08:44 +0200

    fix bug$

--- geocruncher/ComputeIntersections.py
+++ geocruncher/ComputeIntersections.py
@@ -57,8 +57,6 @@ class Slice:
                 xyz = np.stack((x, y, z), axis=-1)
                 xyz.shape = (-1, 3)
                 drillholesLine, springsPoint, matrixGwb = Slice.ouputHydroLayer(np.array([xCoord[0], yCoord[0], zCoord[0]]), np.array([xCoord[1], yCoord[1], zCoord[1]]), xyz, data["springs"], data["drillholes"], meshes_files)
-                drillholesLine, springsPoint, matrixGwb = Slice.ouputHydroLayer(np.array([xCoord[0], yCoord[0], zCoord[0]]), np.array([xCoord[1], yCoord[1], zCoord[1]]), xyz, data["springs"], data["drillholes"], meshes_files)
-                drillholesLine, springsPoint, matrixGwb = Slice.ouputHydroLayer(np.array([xCoord[0], yCoord[0], zCoord[0]]), np.array([xCoord[1], yCoord[1], zCoord[1]]), xyz, data["springs"], data["drillholes"], meshes_files)
         else:
             ySliceRange = np.linspace(yCoord[0], yCoord[1], nPoints)
             z, y = np.meshgrid(zSliceRange, ySliceRange)
@@ -108,7 +106,7 @@ class Slice:
             matrixGwb.append(insidePoints["SelectedPoints"] * gwb_id)  # 0 if not in gwb else gwb_id
         # combine all gwb values into one matrix
         matrixGwbCombine = []
-        if len(matrixGwb) > 1:
+        if len(matrixGwb) > 0:
             for idx, val in enumerate(matrixGwb[0]):
                 values = [values[idx] for values in matrixGwb if values[idx] > 0]
                 matrixGwbCombine.append(int(values[0]) if len(values) > 0 else 0)  # we need to cast to int from int8 to be able to serialise in json
