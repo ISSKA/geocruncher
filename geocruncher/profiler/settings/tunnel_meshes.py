e1024e82f8536853ed8d05b4478e51f8a172802f
Tim Winkelmann <twinkelmann@protonmail.com>
2024-07-17 19:08:00 +0200

    Upgraded all profiler profiles to V2, removing "write_output" step

--- geocruncher/profiler/settings/tunnel_meshes.py
+++ geocruncher/profiler/settings/tunnel_meshes.py
@@ -2,9 +2,9 @@
 # the code will then append the stats to an appropriate file, not mixing between versions
 from ..util import VkProfilerSettings
 
-PROFILER_TUNNEL_MESHES_V1 = VkProfilerSettings(
-    1,
+PROFILER_TUNNEL_MESHES_V2 = VkProfilerSettings(
+    2,
     'tunnel_meshes',
     ['sympy_parse_diff_function', 'interpolate_function',
-        'project_points', 'connect_vertices', 'generate_off', 'write_output'],
+        'project_points', 'connect_vertices', 'generate_off'],
     ['start_time', 'num_waypoints', 'shape'])
