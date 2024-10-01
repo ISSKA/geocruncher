5f5e382b5bcd72156ae9a1fb13b9839e6dee1b2f
Tim Winkelmann <twinkelmann@protonmail.com>
2024-10-01 18:34:00 +0200

    Updated config

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 # This script allows running Geocruncher in local development mode
 # Docker containers use the code on disk, and both the api and worker are setup to hot reload
 set -e
-DOCKER_BUILDKIT=1 docker compose -p geocruncher -f docker/local.docker-compose.yaml up --build --remove-orphans
+DOCKER_BUILDKIT=1 docker compose -p geocruncher-local -f docker/local.docker-compose.yaml up --build --remove-orphans
