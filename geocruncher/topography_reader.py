36e482ae85e6d42fd9e453e086c9f3d62cabca3e
jonas.grueter2 <jonas.grueter@fhnw.ch>
2024-12-17 13:51:03 +0100

    fix formatting

--- geocruncher/topography_reader.py
+++ geocruncher/topography_reader.py
@@ -10,15 +10,16 @@ from io import StringIO
 import numpy as np
 from gmlib.topography_reader import ImplicitDTM
 
+
 def ascii_grid_to_implicit_dtm(dem: str) -> ImplicitDTM:
     """Read ASCIIGrid DEM datapoints and return a GMLIB ImplicitDTM."""
-        
+
     lines = dem.splitlines()
     # We do not actually care about the first 2 line (ncols, nrows), skip them
     xllcorner = float(re.search(r"-?\d+\.?\d*", lines[2])[0])
     yllcorner = float(re.search(r"-?\d+\.?\d*", lines[3])[0])
     cellsize = float(re.search(r"\d+\.?\d*", lines[4])[0])
-    
+
     data_string = '\n'.join(lines[6:])
     zmap = np.loadtxt(StringIO(data_string), dtype=np.float64)
     zmap = zmap[::-1].T
