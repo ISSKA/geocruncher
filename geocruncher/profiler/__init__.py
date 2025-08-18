59ff99d3a589c8067cb8cb3ed6ddb712d0986ba3
jonas.grueter2 <jonas.grueter@fhnw.ch>
2025-08-18 12:08:20 +0200

    add updated profiles to init

--- geocruncher/profiler/__init__.py
+++ geocruncher/profiler/__init__.py
@@ -1,19 +1,19 @@
 from .profiler import VkProfiler, set_profiler, get_current_profiler, profile_step
 from .util import VkProfilerSettings
-from .settings.tunnel_meshes import PROFILER_TUNNEL_MESHES_V2
-from .settings.meshes import PROFILER_MESHES_V3
+from .settings.tunnel_meshes import PROFILER_TUNNEL_MESHES_V3
+from .settings.meshes import PROFILER_MESHES_V4
 from .settings.intersections import PROFILER_INTERSECTIONS_V3
-from .settings.faults import PROFILER_FAULTS_V2
+from .settings.faults import PROFILER_FAULTS_V3
 from .settings.voxels import PROFILER_VOXELS_V2
-from .settings.gwb_meshes import PROFILER_GWB_MESHES_V1
+from .settings.gwb_meshes import PROFILER_GWB_MESHES_V2
 
 PROFILES = {
-    'tunnel_meshes': PROFILER_TUNNEL_MESHES_V2,
-    'meshes': PROFILER_MESHES_V3,
+    'tunnel_meshes': PROFILER_TUNNEL_MESHES_V3,
+    'meshes': PROFILER_MESHES_V4,
     'intersections': PROFILER_INTERSECTIONS_V3,
-    'faults': PROFILER_FAULTS_V2,
+    'faults': PROFILER_FAULTS_V3,
     'voxels': PROFILER_VOXELS_V2,
-    'gwb_meshes': PROFILER_GWB_MESHES_V1
+    'gwb_meshes': PROFILER_GWB_MESHES_V2
 }
 
 __all__ = [
