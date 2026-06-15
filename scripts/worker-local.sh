39e3a145ecd9a40917f6ef8d4449f7f078d67e64
jonas.grueter2 <jonas.grueter@fhnw.ch>
2026-06-15 11:27:58 +0200

    update urllib3 and idna, adapt local scripts to use uv

--- scripts/worker-local.sh
+++ scripts/worker-local.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-watchmedo auto-restart --directory=./ --pattern=*.py --recursive -- celery -A api worker -l INFO -Q geocruncher:long_running,geocruncher:priority
+uv run watchmedo auto-restart --directory=./ --pattern=*.py --recursive -- celery -A api worker -l INFO -Q geocruncher:long_running,geocruncher:priority
